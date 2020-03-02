5dec9f774c638acdd4918c2be44a9461e0f30b2c
Alastair Mucklow <amucklow@strangerpixel.com>
2020-03-02 16:04:48 +0000

    Use factory each time we get a connection

--- Plugin/SegmentPlugin.php
+++ Plugin/SegmentPlugin.php
@@ -32,7 +32,7 @@ class SegmentPlugin
      */
     public function beforeDeleteSegmentCustomers(SegmentResource $segmentResource, Segment $segment)
     {
-        $this->reimportSegmentCustomers($segment);
+        $this->reimportSegmentCustomers($segmentResource, $segment);
     }
 
     /**
@@ -48,24 +48,24 @@ class SegmentPlugin
         $result,
         Segment $segment
     ) {
-        $this->reimportSegmentCustomers($segment);
+        $this->reimportSegmentCustomers($segmentResource, $segment);
         return $result;
     }
 
     /**
+     * @param SegmentResource $segmentResource
      * @param Segment $segment
      */
-    private function reimportSegmentCustomers(Segment $segment)
+    private function reimportSegmentCustomers(SegmentResource $segmentResource, Segment $segment)
     {
-        $segmentResource = $this->segmentResourceFactory->create();
-
-        $customerSegmentQuery = $segmentResource->getConnection()
+        $customerSegmentQuery = $this->segmentResourceFactory->create()
+            ->getConnection()
             ->select()
             ->from($segmentResource->getTable('magento_customersegment_customer'), ['customer_id'])
             ->where('segment_id = ?', $segment->getId())
             ->assemble();
 
-        $segmentResource->getConnection()->update(
+        $this->segmentResourceFactory->create()->getConnection()->update(
             $segmentResource->getTable('email_contact'),
             ['email_imported' => 0],
             ['customer_id IN (?)' => new \Zend_Db_Expr($customerSegmentQuery)]
