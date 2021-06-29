a5c8523055cd93215d5dfb2abebe5654e5a53ebc
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2021-06-29 09:20:16 +0000

    Merged PR 29971: ConfigProvider plugin to send module config to EC
    ## What's being changed
    
    In this PR we added a plugin to send additional ddg enterprise core configuration data to EC
    
    ## Why it's being changed
    
    Because we extending our integration Insight schema so to send additional configuration data
    
    ## How to review / test this change
    
    - Checkout to this branch on Email module and follow the steps of this PR
    https://dev.azure.com/dotdigital/ec/_git/ec-core-magento2-extension/pullrequest/29888?path=%2FModel%2FDotdigitalConfig.php&discussionId=384902
    
    - Confirm that we send enterprise configuration data
    
    Related work items: #144073

---
+++ Plugin/ConfigProviderPlugin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Dotdigitalgroup\Enterprise\Plugin;
+
+use Dotdigitalgroup\Email\Model\Sync\Integration\DotdigitalConfig;
+use Dotdigitalgroup\Enterprise\Helper\Config;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use ReflectionClass;
+
+class ConfigProviderPlugin
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * ConfigProviderPlugin constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @param DotdigitalConfig $subject
+     * @param $result
+     * @param string|int $storeId
+     * @return array
+     */
+    public function afterGetConfigByStore(DotdigitalConfig $subject, $result, $storeId)
+    {
+        foreach ($this->getPaths() as $path) {
+            $keys = explode("/", $path);
+            $configValue = $this->scopeConfig->getValue(
+                $path,
+                ScopeInterface::SCOPE_STORES,
+                $storeId
+            );
+            $result[$keys[0]][$keys[1]][$keys[2]] = (string) $configValue;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    private function getPaths()
+    {
+        $reflectionClass = new ReflectionClass(Config::class);
+        return $reflectionClass->getConstants();
+    }
+}
