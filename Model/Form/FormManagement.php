a1eb019bcb7a100c04d1ca39cfe7563ea95386d5
Shaun Hogan <Shaun.Hogan@dotdigital.com>
2022-06-28 12:52:40 +0000

    Merged PR 38176: Attach embedded form to element specified
    ## What's being changed
    
    Added support for deferred scripts and parameter configurations to form embed and form pope rover scripts.
    
    ## Why it's being changed
    
    Google tag manager and script referral does not interact iwt hthe page builder forms as expected, therefore we needed to modify the way we inject / interact with the dome elements to better support the aforementioned.
    
    ## How to review / test this change
    
    ### Before starting:
    1 Checkout develop branch .
    2. Create a test-old page and add an embedded form to it
    3. Save the form and insure it works as expected.
    
    ### Embed
    1. On Enterprise create a new page.
    2. Inject a embed form and configure accordantly using page builder
    3. Open Test page (incognito widows to insure isolation ) and form behaves as expected in respect to the configurations set.
    4. Insure the Iframe is loaded before the Script and not after within the pagebuilder-video-container
    
    ### Popover
    1. On Enterprise create a new page.
    2. Inject a popover form and configure accordantly using page builder
    3. Open Test page (incognito widows to insure isolation ) and form behaves as expected in respect to the configurations set.
    
    ### After:
    1. Check if your test-old page form still works as expected wien visiting the page.
    
    Related work items: #178219, #178220, #178221, #178666

--- Model/Form/FormManagement.php
+++ Model/Form/FormManagement.php
@@ -2,16 +2,20 @@
 
 namespace Dotdigitalgroup\Enterprise\Model\Form;
 
+use Dotdigitalgroup\Enterprise\Api\Data\FormDataInterface;
+use Dotdigitalgroup\Enterprise\Api\Data\FormDataInterfaceFactory;
+use Dotdigitalgroup\Enterprise\Api\Data\FormOptionInterfaceFactory;
 use Dotdigitalgroup\Enterprise\Api\FormManagementInterface;
 use Dotdigitalgroup\Email\Helper\Data;
+use Magento\Framework\Webapi\Rest\Request;
 use Magento\Store\Model\StoreManagerInterface;
 
 class FormManagement implements FormManagementInterface
 {
-    const EMBEDDED_URL = '/resources/sharing/embed.js';
-    const POPOVER_URL = '/resources/sharing/popover.js';
-    const FORM_SHARING_EMBED = 'lp-embed';
-    const FORM_SHARING_POPOVER = 'lp-popover';
+    public const EMBEDDED_URL = '/resources/sharing/embed.js';
+    public const POPOVER_URL = '/resources/sharing/popover.js';
+    public const FORM_SHARING_EMBED = 'lp-embed';
+    public const FORM_SHARING_POPOVER = 'lp-popover';
 
     /**
      * @var Data
@@ -24,39 +28,43 @@ class FormManagement implements FormManagementInterface
     private $storeManager;
 
     /**
-     * @var \Dotdigitalgroup\Enterprise\Api\Data\FormOptionInterfaceFactory
+     * @var FormOptionInterfaceFactory
      */
     private $formOptionFactory;
 
     /**
-     * @var \Dotdigitalgroup\Enterprise\Api\Data\FormDataInterfaceFactory
+     * @var FormDataInterfaceFactory
      */
     private $formDataFactory;
 
     /**
-     * SurveysForms constructor.
-     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
      * @param Data $data
      * @param StoreManagerInterface $storeManager
-     * @param \Dotdigitalgroup\Enterprise\Api\Data\FormOptionInterfaceFactory $formOptionFactory
-     * @param \Dotdigitalgroup\Enterprise\Api\Data\FormDataInterfaceFactory $formDataFactory
+     * @param FormOptionInterfaceFactory $formOptionFactory
+     * @param FormDataInterfaceFactory $formDataFactory
+     * @param Request $request
      */
     public function __construct(
         Data $data,
         StoreManagerInterface $storeManager,
-        \Dotdigitalgroup\Enterprise\Api\Data\FormOptionInterfaceFactory $formOptionFactory,
-        \Dotdigitalgroup\Enterprise\Api\Data\FormDataInterfaceFactory $formDataFactory
+        FormOptionInterfaceFactory $formOptionFactory,
+        FormDataInterfaceFactory $formDataFactory,
+        Request $request
     ) {
         $this->helper = $data;
         $this->storeManager = $storeManager;
         $this->formOptionFactory = $formOptionFactory;
         $this->formDataFactory = $formDataFactory;
+        $this->request = $request;
     }
 
     /**
-     * @param int $websiteId
-     * @return array|\Dotdigitalgroup\Enterprise\Api\Data\FormOptionInterface[]
-     * @throws \Magento\Framework\Exception\LocalizedException
+     * @inheritDoc
      */
     public function getFormOptions($websiteId)
     {
@@ -69,6 +77,7 @@ class FormManagement implements FormManagementInterface
         $client = $this->helper->getWebsiteApiClient($websiteId);
 
         if ($ECForms = $client->getSurveysAndForms()) {
+            /** @var \stdClass $ECForm */
             foreach ($ECForms as $ECForm) {
                 if (isset($ECForm->id) && $ECForm->state == 'Active' && $this->isNewFormType($ECForm->url)) {
                     $forms[] = $this->formOptionFactory->create()
@@ -82,14 +91,12 @@ class FormManagement implements FormManagementInterface
     }
 
     /**
-     * @param $formId
-     * @param $websiteId
-     * @param $formStyle
-     * @return \Dotdigitalgroup\Enterprise\Api\Data\FormDataInterface|null
+     * @inheritDoc
      */
-    public function getFormData($formId, $websiteId, $formStyle)
+    public function augmentFormData($formId): ?FormDataInterface
     {
-        $client = $this->helper->getWebsiteApiClient($websiteId);
+        $postContent = $this->getPost();
+        $client = $this->helper->getWebsiteApiClient($postContent->account_select);
         $ECForm = $client->getFormById($formId);
 
         if (!$ECForm) {
@@ -97,24 +104,53 @@ class FormManagement implements FormManagementInterface
         }
 
         $pageId = $this->extractPageId($ECForm->url);
-        $domain = $this->extractDomain($ECForm->url);
-        $scriptSrc = $formStyle === 'embedded' ?
-            '//'. $domain . self::EMBEDDED_URL :
-            '//'. $domain . self::POPOVER_URL;
-        $sharing = $formStyle === 'embedded' ? self::FORM_SHARING_EMBED : self::FORM_SHARING_POPOVER;
+        $formAttachment = preg_replace('/[^a-zA-Z0-9_ -]/s', '-', "ddg-form-{$pageId}");
+        $scriptDomain = $this->extractDomain($ECForm->url);
 
+        $scriptSrc = $postContent->form_style === 'embedded' ?
+                    '//'. $scriptDomain . self::EMBEDDED_URL :
+                    '//'. $scriptDomain . self::POPOVER_URL;
+
+        $scriptSharing = ($postContent->form_style === 'embedded') ?
+                    self::FORM_SHARING_EMBED :
+                    self::FORM_SHARING_POPOVER;
+
+        $queryParameters = [
+            'id' => $pageId,
+            'domain' => $scriptDomain,
+            'attach' => "#{$formAttachment}",
+            'delay' => (int) $postContent->show_after,
+            'appearance' =>  $postContent->appearance,
+            'keydismiss' => (bool) $postContent->enable_use_esc,
+            'mobile' => (bool) $postContent->show_mobile,
+            'sharing' => $scriptSharing,
+            'auto' => !($postContent->form_style === 'embedded')
+        ];
+
+        $encodedQueryParameters = http_build_query($queryParameters);
         return $this->formDataFactory->create()
             ->setFormName($ECForm->name)
             ->setFormPageId($pageId)
-            ->setFormDomain($domain)
-            ->setScriptSrc($scriptSrc)
-            ->setFormSharing($sharing);
+            ->setFormDomain($scriptDomain)
+            ->setScriptSrc("{$scriptSrc}?{$encodedQueryParameters}")
+            ->setFormSharing($scriptSharing)
+            ->setFormAttachment($formAttachment);
+    }
+
+    /**
+     * Get post data
+     *
+     * @return \stdClass
+     */
+    private function getPost(): \stdClass
+    {
+        return json_decode($this->request->getContent());
     }
 
     /**
      * Extract the page id from the form URL e.g. 001ln562-3411pu49
      *
-     * @param $url
+     * @param string $url
      * @return string
      */
     private function extractPageId($url)
@@ -126,7 +162,7 @@ class FormManagement implements FormManagementInterface
     /**
      * Extract the domain from the survey URL e.g. rl.dotdigital-pages.com
      *
-     * @param $url
+     * @param string $url
      * @return string
      */
     private function extractDomain($url)
@@ -136,7 +172,9 @@ class FormManagement implements FormManagementInterface
     }
 
     /**
-     * @param $form
+     * Check for type
+     *
+     * @param string $form
      * @return false|int
      */
     private function isNewFormType($form)
